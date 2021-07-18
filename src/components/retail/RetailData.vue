1b4dd78f137dd8691ab042981fd2fe51b5d1ac52
ny0011 <nayun011@gmail.com>
2021-07-18 20:19:00 +0900

    Add Loading page, fake get server request
    - Loading.vue : 서버에서 데이터 받기 전에 보여주는 그림
    - getRetailList.js : 임시 데이터를 비동기로 받아오는 함수

---
+++ src/components/retail/RetailData.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div id="retail-list">
+      <div class="title">
+        <div>미발송 : {{ notShippedAmount }} kg</div>
+      </div>
+      <div class="list-1">
+        <div>미발송</div>
+        <div>발송</div>
+        <div>입금</div>
+        <div>미입금</div>
+      </div>
+      <div class="list-2">
+        <div>주문일자</div>
+        <div>주문자</div>
+        <div>kg</div>
+        <div>간략주소</div>
+        <div>입금여부</div>
+      </div>
+      <div class="list-2" v-for="item in list" v-bind:key="item.id">
+        <div>{{ item.date }}</div>
+        <div>{{ item.name }}</div>
+        <div>{{ item.amount }}</div>
+        <div>{{ item.address }}</div>
+        <div>{{ item.payment }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RetailData",
+  props: ["list", "notShippedAmount"],
+};
+</script>
+
+<style scoped>
+.title,
+.list-1 {
+  display: flex;
+  justify-content: center;
+}
+.list-1 {
+  justify-content: flex-end;
+  margin-right: 4rem;
+}
+.list-1 > div {
+  margin-left: 1rem;
+}
+.list-2 {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+}
+</style>
--- src/components/retail/RetailList.vue
+++ src/components/retail/RetailList.vue
@@ -1,38 +1,70 @@
 <template>
-  <div class="retail-list">
-    <div class="title">
-      <div>미발송 : {{ notShippedAmount }} kg</div>
+  <div>
+    <loading v-if="isloading" />
+    <div v-if="error" class="error">
+      {{ error }}
     </div>
-    <div class="list-1">
-      <div>미발송</div>
-      <div>발송</div>
-      <div>입금</div>
-      <div>미입금</div>
-    </div>
-    <div class="list-2">
-      <div>주문일자</div>
-      <div>주문자</div>
-      <div>kg</div>
-      <div>간략주소</div>
-      <div>입금여부</div>
-    </div>
-    <div class="list-2" v-for="item in list" v-bind:key="item.id">
-      <div>{{ item.date }}</div>
-      <div>{{ item.name }}</div>
-      <div>{{ item.amount }}</div>
-      <div>{{ item.address }}</div>
-      <div>{{ item.payment }}</div>
+    <div id="retail-list" v-if="data !== null">
+      <div class="title">
+        <div>미발송 : {{ data.notShippedAmount }} kg</div>
+      </div>
+      <div class="list-1">
+        <div>미발송</div>
+        <div>발송</div>
+        <div>입금</div>
+        <div>미입금</div>
+      </div>
+      <div class="list-2">
+        <div>주문일자</div>
+        <div>주문자</div>
+        <div>kg</div>
+        <div>간략주소</div>
+        <div>입금여부</div>
+      </div>
+      <div class="list-2" v-for="item in data.list" v-bind:key="item.id">
+        <div>{{ item.date }}</div>
+        <div>{{ item.name }}</div>
+        <div>{{ item.amount }}</div>
+        <div>{{ item.address }}</div>
+        <div>{{ item.payment }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import data from "../tmpdata/RetailList.json";
+import Loading from "../Loading.vue";
+import { getRetailList } from "./getRetailList";
 
 export default {
   name: "RetailList",
   data: function () {
-    return data;
+    return { data: null, isloading: false, error: null };
+  },
+  created() {
+    this.getData();
+  },
+  watch: {
+    $route: "getData",
+  },
+  components: {
+    loading: Loading,
+  },
+  methods: {
+    getData() {
+      this.error = this.data = null;
+      this.isloading = true;
+
+      getRetailList(this.$route, (err, post) => {
+        this.isloading = false;
+        if (err) {
+          this.error = err.toString();
+        } else {
+          this.data = post;
+          //console.log(this.data);
+        }
+      });
+    },
   },
 };
 </script>
