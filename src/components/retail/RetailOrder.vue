1c0767612e7acc56eca1cbf0452ab0ba3bb69365
hyunjiErica <hyunji.erica@gmail.com>
2021-07-25 17:15:42 +0900

    daum post api get zip code through drum post api

--- src/components/retail/RetailOrder.vue
+++ src/components/retail/RetailOrder.vue
@@ -2,8 +2,13 @@
   <div class="order-list">
     <div class="order-text">
       <div>
-        <label for="date"> 주문일 : </label>
-        <input type="text" v-model="date" />
+        <label> 주문일 : </label>
+        <input type="text" v-model="year" size="4" />
+        <label for="year"> 년 </label>
+        <input type="text" v-model="month" size="2" />
+        <label for="month"> 월 </label>
+        <input type="text" v-model="day" size="2" />
+        <label for="day"> 일 </label>
       </div>
       <div>
         <label for="name"> 주문자 : </label>
@@ -21,13 +26,24 @@
         <input type="radio" value="direct" v-model="serve" />
         <label for="direct"> 직접 배송 </label>
       </div>
+      <div v-if="serve == 'express'">
+        <div ref="embed"></div>
+        <label for="zip"> 우편번호 : </label>
+        <input type="text" v-model="zip" size="5" /> &nbsp;
+        <button @click="showApi">검색</button>
+      </div>
       <div>
         <label for="address"> 주소 : </label>
         <input
           type="text"
           v-model="address"
+          size="25"
           placeholder="경기도 화성시 동탄순환대로"
         />
+      </div>
+      <div v-if="serve == 'express'">
+        <label for="address2"> 상세주소: </label>
+        <input type="text" for="address2" placeholder="101동 101호" />
       </div>
       <div>
         <label for="phone"> 휴대폰 번호: </label>
@@ -53,7 +69,10 @@ export default {
   name: "RetailOrder",
   data() {
     return {
-      date: this.getToday(),
+      //date: this.getToday(),
+      year: this.getCurrentYear(),
+      month: this.getCurrentMonth(),
+      day: this.getCurrentDay(),
       name: "",
       amount: "",
       serve: "express",
@@ -61,9 +80,35 @@ export default {
       phone: "",
       errors: [],
       errorShow: false,
+      zip: "",
+      address2: "",
     };
   },
   methods: {
+    showApi: function () {
+      new window.daum.Postcode({
+        oncomplete: (data) => {
+          let fullRoadAddr = data.roadAddress;
+          let extraRoadAddr = "";
+
+          if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+            extraRoadAddr +=
+              extraRoadAddr !== ""
+                ? "," + data.buildingName
+                : data.buildingName;
+          }
+          if (extraRoadAddr !== "") {
+            extraRoadAddr = " (" + extraRoadAddr + ")";
+          }
+          if (fullRoadAddr !== "") {
+            fullRoadAddr += extraRoadAddr;
+          }
+
+          this.zip = data.zonecode;
+          this.address = fullRoadAddr;
+        },
+      }).embed(this.$refs.embed); //.open();
+    },
     validationCheck: function () {
       this.errors = [];
       if (!this.name) {
@@ -73,8 +118,8 @@ export default {
         this.errors.push("주문량을 입력하세요.");
       }
       if (this.serve == "express") {
-        if (!this.address) {
-          this.errors.push("택배의 경우 주소 입력이 필요합니다.");
+        if (!this.zip || !this.addres || !this.address2) {
+          this.errors.push("택배의 경우 우편번호/주소 입력이 필요합니다.");
         }
         if (!this.phone) {
           this.errors.push("택배의 경우 휴대폰 번호 입력이 필요합니다.");
@@ -86,15 +131,22 @@ export default {
       if (this.errors.length == 0) {
         console.log(
           "date : " +
-            this.date +
+            this.year +
+            "-" +
+            this.month +
+            "-" +
+            this.day +
             " name :" +
             this.name +
             " amount :" +
             this.amount +
             " serve :" +
             this.serve +
+            " zip : " +
+            this.zip +
             " address :" +
             this.address +
+            this.address2 +
             " phone :" +
             this.phone
         );
@@ -105,24 +157,31 @@ export default {
       }
     },
     resetData: function () {
-      this.date = this.getToday();
+      //this.date = this.getToday();
+      this.year = this.getCurrentYear();
+      this.month = this.getCurrentMonth();
+      this.day = this.getCurrentDay();
       this.name = "";
       this.amount = "";
       this.serve = "express";
+      this.zip = "";
       this.address = "";
+      this.address2 = "";
       this.phone = "";
       this.errors = [];
       this.errorShow = false;
     },
-    getToday: function () {
+    getCurrentMonth: function () {
       const today = new Date();
-      const day =
-        today.getFullYear() +
-        "-" +
-        (today.getMonth() + 1) +
-        "-" +
-        today.getDate();
-      return day;
+      return today.getMonth() + 1;
+    },
+    getCurrentYear: function () {
+      const today = new Date();
+      return today.getFullYear();
+    },
+    getCurrentDay: function () {
+      const today = new Date();
+      return today.getDate();
     },
   },
 };
