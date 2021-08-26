87746655e34897b84bbeee251f9ff2d391fa4175
ny0011 <nayun011@gmail.com>
2021-08-26 22:23:22 +0900

    change proxy api

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   devServer: {
     proxy: {
-      "/retail": {
+      "/": {
         target: "http://35.223.247.96",
         changeOrigin: true,
         logLevel: "debug",
-      },
+      }
     },
   },
 };
